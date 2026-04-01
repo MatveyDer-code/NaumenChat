cf81727adee38af2d3d3612b0e3a273a38c9e55c
matvejIDer <derebezovm@mail.ru>
2026-04-01 23:06:38 +0700

    Restrict Data REST endpoints to ADMIN role with common /data prefix

--- src/main/java/ru/matveyder/NauJava/security/SecurityConfig.java
+++ src/main/java/ru/matveyder/NauJava/security/SecurityConfig.java
@@ -28,7 +28,8 @@ public class SecurityConfig {
                         .requestMatchers(
                                 "/swagger-ui/**",
                                 "/swagger-ui.html",
-                                "/v3/api-docs/**"
+                                "/v3/api-docs/**",
+                                "/data/**"
                         ).hasRole("ADMIN")
                         .anyRequest().authenticated()
                 )
