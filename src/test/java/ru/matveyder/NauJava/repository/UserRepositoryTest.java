ce03c612cbb20070b3e458980363974d0e430ed2
matvejIDer <derebezovm@mail.ru>
2026-04-15 21:00:43 +0700

    Update repository tests

--- src/test/java/ru/matveyder/NauJava/repository/UserRepositoryTest.java
+++ src/test/java/ru/matveyder/NauJava/repository/UserRepositoryTest.java
@@ -3,8 +3,8 @@ package ru.matveyder.NauJava.repository;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
-import org.springframework.boot.jdbc.test.autoconfigure.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import ru.matveyder.NauJava.entity.Role;
 import ru.matveyder.NauJava.entity.User;
 
