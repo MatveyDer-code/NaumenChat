91c20f496e0ac86a96d09eadd6575a4d0ca0416a
matvejIDer <derebezovm@mail.ru>
2026-04-15 20:59:42 +0700

    Added message dto

--- src/main/java/ru/matveyder/NauJava/entity/DTO/MessageDto.java
+++ src/main/java/ru/matveyder/NauJava/entity/DTO/MessageDto.java
@@ -1,4 +1,9 @@
 package ru.matveyder.NauJava.entity.DTO;
 
-public record MessageDto() {
-}
+public record MessageDto(
+        Long id,
+        String content,
+        Long authorId,
+        Long chatRoomId,
+        Boolean isRead
+) {}
